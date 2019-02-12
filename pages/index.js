d7afd0c3753e00abae75b1e328c2a8460c9ce8d3
Luis Bosnembiante <luisbonsembiante@gmail.com>
2019-02-12 15:31:24 -0300

    init

--- pages/index.js
+++ pages/index.js
@@ -2,33 +2,32 @@ import React from 'react';
 import {Card, Button, Confirm} from 'semantic-ui-react';
 import Layout from '../components/Layout';
 import {Link} from '../routes';
-import factory from '../ethereum/factory';
 
 export default class CampaignIndex extends React.Component {
+
 
 
     // Initial PROPS
     static async getInitialProps() {
 
-
+        return {};
     }
 
     render() {
         return (
 
-            < Layout >
-            < div >
-            < link
-        rel = "stylesheet"
-        href = "//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
-            / >
+            <Layout>
+                <div>
+                    <link
+                        rel="stylesheet"
+                        href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
+                    />
 
-            < h3 > Home < /h3>
+                    <h3>Home</h3>
 
-            < /div>
+                </div>
 
-            < /Layout>
-    )
-        ;
+            </Layout>
+        );
     }
 }
