3e5d06854bb7536f46b62bc6a65fab059d510352
Federico Madoery <fede.madoery@hotmail.com>
2019-02-13 16:53:35 -0300

    Better inprove for move menu items

--- components/dash/MenuItems.js
+++ components/dash/MenuItems.js
@@ -8,22 +8,6 @@ const _menuItems = (props) => {
 
     const [start, setStart] = useState(0);
     const [end, setEnd] = useState(0);
-    const items = [
-        {name: 'JavaScript'},
-        {name: 'Java'},
-        {name: 'JavaScript 2'},
-        {name: 'Java 2'},
-        {name: 'JavaScript 3'},
-        {name: 'Java 3'},
-        {name: 'JavaScript 4'},
-        {name: 'Java 4'},
-        {name: 'JavaScript 5'},
-        {name: 'Java 5'},
-        {name: 'JavaScript 6'},
-        {name: 'Java 6'},
-        {name: 'JavaScript 7'},
-        {name: 'Java 7'}
-    ]; // TODO get this from Redux
 
     const LENGTH_TO_SHOW = 7; // Cant of menu items to show
     const moveItems = (cant) => {
@@ -34,12 +18,10 @@ const _menuItems = (props) => {
     // TODO Do better implementation of arrows - Fixed position
     return (
         <>
-            {(start > 0) && (
-                <Menu.Item name='more' onClick={() => moveItems(-1)}>
-                    <Label color='teal'>{'+ ' + (start)}</Label>
-                    <Icon name='arrow left'/>
-                </Menu.Item>
-            )}
+            <Menu.Item name='more' onClick={() => moveItems(-1)}>
+                {start > 0 && <Label color='teal'>{'+ ' + (start)}</Label>}
+                <Icon name='arrow left'/>
+            </Menu.Item>
             {_.slice(props.items, 0 + start, LENGTH_TO_SHOW + end).map((item, index) =>
                 <Menu.Item
                     key={item + index}
@@ -47,12 +29,12 @@ const _menuItems = (props) => {
                     active={props.activeItem === item.name}
                     onClick={props.handleItemClick}/>
             )}
-            {((end + LENGTH_TO_SHOW) !== items.length) && (
-                <Menu.Item name='more' onClick={() => moveItems(1)}>
-                    <Icon name='arrow right'/>
-                    <Label color='teal'>{'+ ' + (items.length - LENGTH_TO_SHOW - end)}</Label>
-                </Menu.Item>
-            )}
+            <Menu.Item name='more' onClick={() => moveItems(1)}>
+                <Icon name='arrow right'/>
+                {((end + LENGTH_TO_SHOW) < props.items.length) &&
+                <Label color='teal'>{'+ ' + (props.items.length - LENGTH_TO_SHOW - end)}</Label>
+                }
+            </Menu.Item>
         </>
     )
 };
