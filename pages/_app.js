6b7aa2b0688a3a3a0634fe0a512705162a46c499
Luis Bosnembiante <luisbonsembiante@gmail.com>
2019-02-13 14:03:51 -0300

    react es una bosta

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,37 @@
-import App, { Container } from 'next/app'
+import App, {Container} from 'next/app'
 import React from 'react'
 import withReduxStore from '../lib/with-redux-store'
-import { Provider } from 'react-redux'
+import {Provider} from 'react-redux'
+import firebase from '@firebase/app'
+import {apiKey, authDomain, databaseURL, messagingSenderId, projectId, storageBucket} from "../env";
 
 class SkillMatrix extends App {
-  render () {
-    const { Component, pageProps, reduxStore } = this.props
-    return (
-      <Container>
-        <Provider store={reduxStore}>
-          <Component {...pageProps} />
-        </Provider>
-      </Container>
-    )
-  }
+
+    componentDidMount() {
+
+        let config = {
+            apiKey ,
+            authDomain,
+            databaseURL,
+            projectId,
+            storageBucket,
+            messagingSenderId
+        };
+        firebase.initializeApp(config);
+
+
+    }
+
+    render() {
+        const {Component, pageProps, reduxStore} = this.props
+        return (
+            <Container>
+                <Provider store={reduxStore}>
+                    <Component {...pageProps} />
+                </Provider>
+            </Container>
+        )
+    }
 }
 
 export default withReduxStore(SkillMatrix)
