4bd01a28654b7f2b3f8cf785a73c35fcc6e076c8
Federico Madoery <fede.madoery@hotmail.com>
2019-02-14 10:46:03 -0300

    Header router

--- components/commons/Header.js
+++ components/commons/Header.js
@@ -1,40 +1,42 @@
 import React from 'react';
-import { Menu } from 'semantic-ui-react';
-import { Link } from '../../routes';
+import {Button, Menu} from 'semantic-ui-react';
+import {Link, Router} from '../../routes';
 import {connect} from "react-redux";
+import {withRouter} from "next/router";
 import {skillCreate} from "../../redux/actions";
 
-const _header  =  (props) => {
+const _header = (props) => {
     //state = {}
 
+    const ROUTES = {
+        home: '/index',
+        skills: '/skills'
+    };
 
-   // handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
+    const handleItemClick = (e, {name}) => Router.pushRoute(ROUTES[name.toLowerCase()]);
     const onClick = () => {
-         props.skillCreate( {name:'Java', description:'dfasdfasdf'} );
-    }
+        props.skillCreate({name: 'Java', description: 'dfasdfasdf'});
+    };
 
     return (
-        <Menu style={{ marginTop: '10px' }}>
-            <Link route="/index">
-                <a className="item">Home</a>
-            </Link>
+        <Menu style={{marginTop: '10px'}} size='small'>
+            <Menu.Item active={ROUTES.home === props.router.asPath}
+                       onClick={handleItemClick} name='Home' />
 
             <Menu.Menu position="right">
-                <Link route="/index">
-                    <a className="item">Skill</a>
-                </Link>
+                <Menu.Item active={ROUTES.skills === props.router.asPath}
+                           onClick={handleItemClick} name='Skills' />
 
-
-                    <a className="item" onClick={onClick}>+</a>
-               
+                <a className="item" onClick={onClick}>+</a>
             </Menu.Menu>
         </Menu>
     );
 }
 
 
-const  mapStateToProps = (state) => {
-    return {}
+const mapStateToProps = (state) => {
+    return {...state.router}
 }
 
-export default connect(mapStateToProps, {skillCreate})(_header);
+export default withRouter(connect(mapStateToProps, {skillCreate})(_header));
