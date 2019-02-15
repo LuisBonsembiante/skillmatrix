64eb36f1ef73f3f5b5951d7bb98fd534856aa79a
Federico Madoery <fede.madoery@hotmail.com>
2019-02-15 13:57:31 -0300

    Cards.js now is a class component - To fix a bug with technologies fech and skillSelected change

--- components/dash/Cards.js
+++ components/dash/Cards.js
@@ -1,129 +1,128 @@
-import React, {useReducer} from "react";
+import React, {Component} from "react";
 import {connect} from "react-redux";
 import {Card, Dropdown, Icon, Label, Popup} from 'semantic-ui-react';
 import {getSmallImage} from "../utils/imagesManager";
 import _ from "lodash";
 
-const _cards = (props) => {
+class Cards extends Component {
 
-
-    const skillSelected = _.find(props.skills, ['name', props.skillSelected]);
-
-    const technologies = (skillSelected && skillSelected.technologies)
-        ? Object.values(skillSelected.technologies).map(
-            (item, index) => {
-                return { // TODO get the real user information an put the approve state into technologies
-                    ...item,
-                    validated: !(index % 2),
-                    validator: {
-                        name: 'Helen',
-                        position: 'Co-Worker'
-                    },
-                    levelOfKnowledge: {}
-                }
-            })
-        : [{name: '', meta: '', description: 'Technologies not found'}];
-
-    //Init state with the selected Technologies
-    const [state, dispatch] = useReducer(reducer, {technologies});
-
-    const onOptionClick = (index, value) => {
-        dispatch({type: 'update_knowledge', payload: value, index: index});
+    state = {
+        technologies: []
     };
 
-    console.log(state)
-    return (
-        <Card.Group itemsPerRow={4}>
-            {state.technologies.map((item, index) =>
-                <Card key={item + index}>
-                    <Card.Content>
-                        <Card.Header>{item.name}</Card.Header>
-                        <Card.Meta>
-                            <span className='date'>{item.meta}</span>
-                        </Card.Meta>
-                        <Card.Description>{item.description}</Card.Description>
-
-                        <br/>
-                        {item.levelOfKnowledge &&
-                        <Dropdown text={item.levelOfKnowledge.text || 'Level of knowledge'}
-                                  icon='certificate' className={'icon ' + (item.levelOfKnowledge.color || 'grey')}
-                                  floating labeled button basic
-                                  value={item.levelOfKnowledge.value}>
-                            <Dropdown.Menu>
-                                <Dropdown.Item label={{color: 'green', empty: true, circular: true}}
-                                               text='I want to learn this' value={0}
-                                               onClick={(e, {text, value, label}) =>
-                                                   onOptionClick(index, {text, value, color: label.color})
-                                               }
-                                />
-                                <Dropdown.Item label={{color: 'teal', empty: true, circular: true}}
-                                               text='Basic' value={1}
-                                               onClick={(e, {text, value, label}) =>
-                                                   onOptionClick(index, {text, value, color: label.color})
-                                               }
-                                />
-                                <Dropdown.Item label={{color: 'blue', empty: true, circular: true}}
-                                               text='Intermediate' value={2}
-                                               onClick={(e, {text, value, label}) =>
-                                                   onOptionClick(index, {text, value, color: label.color})
-                                               }
-                                />
-                                <Dropdown.Item label={{color: 'violet', empty: true, circular: true}}
-                                               text='Advanced' value={3}
-                                               onClick={(e, {text, value, label}) =>
-                                                   onOptionClick(index, {text, value, color: label.color})
-                                               }
-                                />
-                            </Dropdown.Menu>
-                        </Dropdown>
-                        }
-
-                    </Card.Content>
-                    {item.validator &&
-                    <Card.Content extra>
-                        <Popup trigger={
-                            <Label as='a' color={item.validated ? 'green' : 'yellow'} image>
-                                <img src={getSmallImage()}/>
-                                {item.validator.name}
-                                <Label.Detail>{item.validator.position}</Label.Detail>
-                            </Label>
-                        } content={item.validated ? 'Validated!' : 'Pending for validation'}/>
-                    </Card.Content>
-                    }
-                </Card>
-            )}
-        </Card.Group>
-    )
-};
-
-/**
- * Redux Pattern implemented to managed the state
- */
-
-const initialState = {technologies: []};
-
-function reducer(state, action) {
-    switch (action.type) {
-        case 'reset':
-            return initialState;
-        case 'update_knowledge':
-            return {
-                ...state,
-                technologies: state.technologies.map(
-                    (content, i) => i === action.index
-                        ? {...content, levelOfKnowledge: action.payload}
-                        : content
-                )
-            };
-        default:
-            return state;
+    componentDidMount() {
+        const {skillSelected} = this.props;
+        this.getSkillTechnologies(skillSelected)
     }
-}
 
-/**
- * End Redux Pattern
- */
+    getSkillTechnologies = (skillSelected) => {
+        const {skills} = this.props;
 
+        const _skillSelected = _.find(skills, ['name', skillSelected]);
+
+        const _technologies = (_skillSelected && _skillSelected.technologies)
+            ? Object.values(_skillSelected.technologies).map(
+                (item, index) => {
+                    return { // TODO get the real user information an put the approve state into technologies
+                        ...item,
+                        validated: !(index % 2),
+                        validator: {
+                            name: 'Helen',
+                            position: 'Co-Worker'
+                        },
+                        levelOfKnowledge: {}
+                    }
+                })
+            : [{name: '', meta: '', description: 'Technologies not found'}];
+
+        this.setState({technologies: _technologies})
+    };
+
+    componentWillReceiveProps(nextProps, nextContext) {
+        const {skillSelected} = this.props;
+        if(skillSelected !== nextProps.skillSelected) {
+            this.getSkillTechnologies(nextProps.skillSelected);
+        }
+    }
+
+    onOptionClick = (index, value) => {
+        this.setState((state) => {return {
+            ...state,
+            technologies: state.technologies.map(
+                (content, i) => i === index
+                    ? {...content, levelOfKnowledge: value}
+                    : content
+            )
+        }});
+    };
+
+    render() {
+        const {technologies} = this.state;
+
+        return (
+            <Card.Group itemsPerRow={4}>
+                {technologies.map((item, index) =>
+                    <Card key={item + index}>
+                        <Card.Content>
+                            <Card.Header>{item.name}</Card.Header>
+                            <Card.Meta>
+                                <span className='date'>{item.meta}</span>
+                            </Card.Meta>
+                            <Card.Description>{item.description}</Card.Description>
+
+                            <br/>
+                            {item.levelOfKnowledge &&
+                            <Dropdown text={item.levelOfKnowledge.text || 'Level of knowledge'}
+                                      icon='certificate' className={'icon ' + (item.levelOfKnowledge.color || 'grey')}
+                                      floating labeled button basic
+                                      value={item.levelOfKnowledge.value}>
+                                <Dropdown.Menu>
+                                    <Dropdown.Item label={{color: 'green', empty: true, circular: true}}
+                                                   text='I want to learn this' value={0}
+                                                   onClick={(e, {text, value, label}) =>
+                                                       this.onOptionClick(index, {text, value, color: label.color})
+                                                   }
+                                    />
+                                    <Dropdown.Item label={{color: 'teal', empty: true, circular: true}}
+                                                   text='Basic' value={1}
+                                                   onClick={(e, {text, value, label}) =>
+                                                       this.onOptionClick(index, {text, value, color: label.color})
+                                                   }
+                                    />
+                                    <Dropdown.Item label={{color: 'blue', empty: true, circular: true}}
+                                                   text='Intermediate' value={2}
+                                                   onClick={(e, {text, value, label}) =>
+                                                       this.onOptionClick(index, {text, value, color: label.color})
+                                                   }
+                                    />
+                                    <Dropdown.Item label={{color: 'violet', empty: true, circular: true}}
+                                                   text='Advanced' value={3}
+                                                   onClick={(e, {text, value, label}) =>
+                                                       this.onOptionClick(index, {text, value, color: label.color})
+                                                   }
+                                    />
+                                </Dropdown.Menu>
+                            </Dropdown>
+                            }
+
+                        </Card.Content>
+                        {item.validator &&
+                        <Card.Content extra>
+                            <Popup trigger={
+                                <Label as='a' color={item.validated ? 'green' : 'yellow'} image>
+                                    <img src={getSmallImage()}/>
+                                    {item.validator.name}
+                                    <Label.Detail>{item.validator.position}</Label.Detail>
+                                </Label>
+                            } content={item.validated ? 'Validated!' : 'Pending for validation'}/>
+                        </Card.Content>
+                        }
+                    </Card>
+                )}
+            </Card.Group>
+        )
+    }
+};
 
 function mapStateToProps(state) {
     const skills = _.map(state.fireBase.skills, (val, uid) => {
@@ -136,4 +135,4 @@ function mapStateToProps(state) {
     };
 }
 
-export default connect(mapStateToProps, {})(_cards);
+export default connect(mapStateToProps, {})(Cards);
