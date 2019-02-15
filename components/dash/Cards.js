23f504959cd1405be37e1f43c967cac7e328fb6f
Federico Madoery <fede.madoery@hotmail.com>
2019-02-15 10:26:00 -0300

    Cards.js now render real techonologies

--- components/dash/Cards.js
+++ components/dash/Cards.js
@@ -2,55 +2,49 @@ import React from "react";
 import {connect} from "react-redux";
 import {Card, Icon, Label, Popup} from 'semantic-ui-react';
 import {getSmallImage} from "../utils/imagesManager";
+import _ from "lodash";
 
 const _cards = (props) => {
 
-    const items = [
-        {
-            header: 'Angular Js',
-            meta: 'Addres of Manager',
-            description: 'The manager created this campaign and can create request to withdraw money',
-            validated: false,
-            validator: {
-                name: 'Helen',
-                position: 'Co-Worker'
-            },
-            style: {overflowWrap: 'break-word'}
-        },
-        {
-            header: 'React Js',
-            meta: 'Minimum Contribution (wei)',
-            description: 'You must contribute at least this much wei',
-            validated: true,
-            validator: {
-                name: 'Shuan',
-                position: 'Teacher'
-            },
-            style: {overflowWrap: 'break-word'}
-        }
-    ]; // TODO Get this items from Redux
 
+    const skillSelected = _.find(props.skills, ['name', props.skillSelected]);
+
+    const technologies = (skillSelected && skillSelected.technologies)
+        ? Object.values(skillSelected.technologies).map(
+            (item, index) => {
+                return { // TODO get the real user information an put the approve state into technologies
+                    ...item,
+                    validated: !(index % 2),
+                    validator: {
+                        name: 'Helen',
+                        position: 'Co-Worker'
+                    }
+                }
+            })
+        : [{name: '', meta: '', description: 'Technologies not found'}];
 
     return (
         <Card.Group itemsPerRow={4}>
-            {items.map((item, index) =>
+            {technologies.map((item, index) =>
                 <Card key={item + index}>
                     <Card.Content>
-                        <Card.Header>{item.header}</Card.Header>
+                        <Card.Header>{item.name}</Card.Header>
                         <Card.Meta>
                             <span className='date'>{item.meta}</span>
                         </Card.Meta>
                         <Card.Description>{item.description}</Card.Description>
                     </Card.Content>
-                    <Card.Content extra>
-                        <Popup trigger={
-                            <Label as='a' color={item.validated ? 'green' : 'yellow'} image>
-                                <img src={getSmallImage()} />
-                                {item.validator.name}
-                                <Label.Detail>{item.validator.position}</Label.Detail>
-                            </Label>
-                        }  content={item.validated ? 'Validated!' : 'Pending for validation'} />
-                    </Card.Content>
+                    {   item.validator &&
+                        <Card.Content extra>
+                            <Popup trigger={
+                                <Label as='a' color={item.validated ? 'green' : 'yellow'} image>
+                                    <img src={getSmallImage()}/>
+                                    {item.validator.name}
+                                    <Label.Detail>{item.validator.position}</Label.Detail>
+                                </Label>
+                            } content={item.validated ? 'Validated!' : 'Pending for validation'}/>
+                        </Card.Content>
+                    }
                 </Card>
             )}
         </Card.Group>
@@ -58,7 +52,14 @@ const _cards = (props) => {
 };
 
 function mapStateToProps(state) {
-    return {}
+    const skills = _.map(state.fireBase.skills, (val, uid) => {
+        return {...val, key: uid, title: ''}; // {shift: 'Monday', name:'s', id:'1j2j34'};
+    });
+
+    return {
+        skills,
+        loading: state.fireBase.loading
+    };
 }
 
 export default connect(mapStateToProps, {})(_cards);
