49cd53401e1bd81dc31a65312aa90d85cc1e5ff0
Luis Bosnembiante <luisbonsembiante@gmail.com>
2019-02-19 11:39:15 -0300

    now

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import App, {Container} from 'next/app'
+import React from 'react'
+
+
+class _document extends App {
+
+    render() {
+
+        return (
+           <></>
+
+        )
+    }
+}
+
+export default _document
---
+++ pages/_error.js
@@ -0,0 +1,16 @@
+import App, {Container} from 'next/app'
+import React from 'react'
+
+
+class _error extends App {
+
+    render() {
+
+        return (
+            <></>
+
+        )
+    }
+}
+
+export default _error
