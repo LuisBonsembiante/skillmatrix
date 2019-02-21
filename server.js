91c6ae4f323cda629d2ba1a32567e19bf050b899
Luis Bosnembiante <luisbonsembiante@gmail.com>
2019-02-21 15:54:24 -0300

    now

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const {createServer} = require('http');
 const next = require('next')
 
 const app = next({
-    dev: process.env.NODE_ENV !== 'production'
+    dev: true
 });
 
 const routes = require('./routes');
