7095635c9b4f536f673178ae83338245e20672ff
Luis Bosnembiante <luisbonsembiante@gmail.com>
2019-02-28 15:30:36 -0300

    cards employees

---
+++ ethereum/contracts/Stage.sol
@@ -0,0 +1,60 @@
+contract Stage is Ownable {
+
+    mapping(uint => uint[]) public tokenToHitos;
+    mapping(uint => string) public hitoType;
+
+    struct Hito {
+        uint tipo; // Referencia a un usuario
+        bool aprovado; // Referencia a una tecnologia
+    }
+
+    uint hitosCount = 0;
+
+    Hito[] public hitoList;
+
+    // Registra un nuevo token, y le asigna todos los hitos como no aprovados
+    function registerToken(uint _tokeId) internal {
+        for (uint i = 1; i <= hitosCount; i++) {
+            uint hitoId = hitoList.push(Hito(i, false)) - 1;
+            tokenToHitos[_tokeId].push(hitoId);
+        }
+    }
+
+    // Retorna el estado de un tipo de hito, relacionado al token
+    function getStateOfToken(uint _tokeId, uint _hitoType) external onlyOwner returns (Hito) {
+        if(getTokenToHitosLength(_toke_id) == 0 ) {
+            registerToken(_toke_id);
+        }
+        uint hitoId = tokenToHitos[_tokeId][_hitoType];
+        return hitoList[hitoId];
+    }
+
+    // Agrega un nuevo hito al stage
+    function addHito(string _tipo) external onlyOwner {
+        hitosCount++;
+        hitoType[hitosCount] = _tipo;
+    }
+
+    // Cambia el estado de un hito, relacionado a un token
+    function changeHitoState(uint _tokeId, uint _hitoType, bool _state) external onlyOwner {
+        uint hitoId = tokenToHitos[_tokeId][_hitoType];
+        hitoList[hitoId].aprovado = _state;
+    }
+
+    // Retorna la lista de ID de los hitos relacionados al token
+    function getTokenToHitos(uint _tokeId) public view onlyOwner returns (uint[]) {
+        return tokenToHitos[_tokeId];
+    }
+
+    // Retorna el largo de la lista de ID de los hitos relacionados al token
+    function getTokenToHitosLength(uint _tokeId) public view onlyOwner returns (uint) {
+        return tokenToHitos[_tokeId].length;
+    }
+
+    // Retorna un tipo de hito relacionado al token
+    function getTokenToHitosValue(uint _tokeId, uint _hitoType) public view onlyOwner returns (uint) {
+        return tokenToHitos[_tokeId][_hitoType];
+    }
+
+
+}
