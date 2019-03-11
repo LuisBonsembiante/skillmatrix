1e20b18f627542848009f9fec3168ea993891e60
Federico Madoery <fede.madoery@hotmail.com>
2019-03-11 10:15:49 -0300

    Dlete ethereum folder

--- ethereum/stage.js
+++
@@ -1,9 +0,0 @@
-import web3 from "./web3";
-import Stage from  './build/Stage.json';
-
-export default (address) => {
-    return new web3.eth.Contract(
-        JSON.parse(Stage.interface),
-        address
-    );
-};
