90c462ac1b36cb50b9c2aa3f698b6ff10f2e9fad
Federico Madoery <fede.madoery@hotmail.com>
2019-03-21 16:34:00 -0300

    New env.js for public release

--- env.js
+++ env.js
@@ -1,7 +1,7 @@
-export const apiKey = "AIzaSyAbWTIcgRWEJQivARnkKhRFlMRnbpeXlwc";
-export const authDomain = "skill-matrix-b30f5.firebaseapp.com";
-export const databaseURL = "https://skill-matrix-b30f5.firebaseio.com";
-export const projectId = "skill-matrix-b30f5";
-export const storageBucket = "skill-matrix-b30f5.appspot.com";
-export const messagingSenderId = "154488837778";
+export const apiKey = "AIzaSyC5_7kEFp1vOI7OOnPuFk1vT0jz-HOD-dg";
+export const authDomain = "skillmatrix-public.firebaseapp.com";
+export const databaseURL = "https://skillmatrix-public.firebaseio.com";
+export const projectId = "skillmatrix-public";
+export const storageBucket = "skillmatrix-public.appspot.com";
+export const messagingSenderId = "953235239147";
 export const getUserByEmail = "https://us-central1-skill-matrix-b30f5.cloudfunctions.net/getUserByEmail";
